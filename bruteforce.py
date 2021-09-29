fe0fdf5a6866e719808dae18a80f23dfa57aa0df
tom gauthier <tomgauthier0@gmail.com>
2021-09-29 17:29:46 +0200

    bruteforce algorithm working, taking about 10 seconds

--- bruteforce.py
+++ bruteforce.py
@@ -1,84 +1,40 @@
 from itertools import combinations, product
 import pandas as pd
-# Create a table with the actions
-
-# Actions, Coût par action (en euros), Bénéfice (après 2 ans)
-column_names = ['action id', 'action cost', '2y benefit (%)']
-df = pd.DataFrame([
-    [1,  20,  5],
-    [2,  30,  10],
-    [3,  50,  15],
-    [4,  70,  20],
-    [5,  60,  17],
-    [6,  80,  25],
-    [7,  22,  7],
-    [8,  26,  11],
-    [9,  48,  13],
-    [10, 34,  27],
-    [11, 42,  17],
-    [12, 110,  9],
-    [13, 38,  23],
-    [14, 14,  1],
-    [15, 18,  3],
-    [16, 8,  8],
-    [17, 4,  12],
-    [18,   10,  14],
-    [19, 24,  21],
-    [20, 114, 18]], columns=column_names)
-
-# Define the ROI of each action and add it to the table
-# Create a column with the value of each action after 2 years
-df['value after 2y'] = df['action cost'] * ((df['2y benefit (%)']*0.01)+1)
-print(df)
+import time
 
 
-# Go through the actions one by one
-# While budget is < 500€
-# add the next action
-#
-
-# all I want is all the combinaitions of action cost under 500€
-
-numbers = df['action cost'].tolist()
-
-# result = [seq for i in range(len(numbers), 0, -1) for seq in itertools.combinations(numbers, i) if sum(seq) <= 500]
-result = []
-
-my_dict = df['action cost'].to_dict()
-
-for i in range(len(my_dict)):
-    print("i: ", i)
-    for index, value in list(combinations(df.index, 2)):
-        print(index)
-        print(value)
-        # print(df.loc[index]['action cost'])
-        # print(df.loc[index, :])
-        # print(sum(df.loc[index, :]))
-        # print('\n')
-
-    for key, value in combinations(values, i):
-        # for seq in combinations(df['action cost'], i):
-        # print(seq)
-        # if sum(seq) <= 50:
-        # result.append(seq)
-        dict(zip(keys, value))
+start_time = time.time()
 
 
-my_dict = {'A': ['D', 'E'], 'B': ['F', 'G', 'H'], 'C': ['I', 'J']}
-my_dict = {'1': 20, '2': 30, '3': 50, '4': 70}
-keys, values = zip(*my_dict.items())
-permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
+def main():
+    column_names = ['action id', 'action cost', '2y benefit (%)']
+    df = pd.read_csv("stock_list.csv", index_col=False, names=column_names)
 
-# https://realpython.com/lessons/how-use-itertools-dictionaries/
-# https://www.geeksforgeeks.org/group-list-of-dictionary-data-by-particular-key-in-python/
-rows = df.to_dict('records') # list of dicts
+    df['2y value'] = df['action cost'] + \
+        (df['action cost'] * df['2y benefit (%)']/100)
+    df['2y ROI'] = df['2y value'] - df['action cost']
+    options = []
+    stocks_dict = df.to_dict('records')
+    for L in range(0, len(stocks_dict)+1):
+        for subset in combinations(stocks_dict, L):
+            stocks = list(subset)
+            # type(stocks[2]['cost'])
+            cost = sum(stock['action cost'] for stock in stocks)
+            value = sum(stock['2y value'] for stock in stocks)
+            roi = sum(stock['2y ROI'] for stock in stocks)
+            stocks_ids = [stock['action id'] for stock in stocks]
+            option = {'totalcost': cost,
+                      '2 Year ROI': roi,
+                      'Stocks IDs': stocks_ids}
+            if cost < 500:
+                options.append(option)
+    df_options = pd.DataFrame(
+        options, columns=['totalcost', '2 Year ROI', 'Stocks IDs'])
+    print(df_options.sort_values(by=['2 Year ROI'], ascending=False))
+    df_options.to_csv('output_options.csv')
 
-def key_func(k):
-    return k['action cost']
 
+if __name__ == "__main__":
+    main()
 
-for key, value in combinations(rows, key_func)
-
-
-
-
+print("--- %s seconds ---" % (time.time() - start_time))
