9bd89448f11c1a9874e541dc4d70beabd3602b9f
tom gauthier <tomgauthier0@gmail.com>
2021-10-08 14:36:07 +0200

    solutions for bruteforce and optimized done

--- bruteforce.py
+++ bruteforce.py
@@ -9,8 +9,6 @@ def main():
         if n == 0:
             return [[]]
         l = []
-        # print("lst: ", lst)
-        print("len(lst): ", len(lst))
         for i in range(0, len(lst)):  # 0,1,2,3
             # print("   lst[i]: ", lst[i])
             m = lst[i]  # A
@@ -29,11 +27,7 @@ def main():
     df = pd.read_csv("stock_list.csv", index_col=False, names=column_names)
 
     # create a column for the value of the action after 2 years
-    df['2y value'] = df['action cost'] + \
-        (df['action cost'] * df['2y benefit (%)']/100)
-
-    # Create a column showing the Return On Investment (ROI) for each action
-    df['2y ROI'] = df['2y value'] - df['action cost']
+    df['2y value'] = df['action cost'] * df['2y benefit (%)']/100
 
     # Instantiate an emplty array options to store the different combinations
     # of stocks
@@ -49,18 +43,17 @@ def main():
         # Get the subset of combinations of stocks of length L
 
         for subset in n_length_combo(stocks_dict, L):
-            print("SUBSET: ", subset)
             # Convert the returned tuple to an a list
             stocks = list(subset)
             # for each subset of stocks, calculate the sum of costs and ROI
             cost = sum(stock['action cost'] for stock in stocks)
-            roi = sum(stock['2y ROI'] for stock in stocks)
+            value = sum(stock['2y value'] for stock in stocks)
             # Create a list of the stocks ids
             stocks_ids = [stock['action id'] for stock in stocks]
             # Create a dict called option with the cost, roi and stocks IDs
             # An option is a combination of stocks
             option = {'totalcost': cost,
-                      '2 Year ROI': roi,
+                      '2Y value': value,
                       'Stocks IDs': stocks_ids}
             # If the total cost of the option is less than 500, add it to the
             # options list
@@ -68,11 +61,11 @@ def main():
                 options.append(option)
     # Convert the options list to a pandas dataframe
     df_options = pd.DataFrame(
-        options, columns=['totalcost', '2 Year ROI', 'Stocks IDs'])
+        options, columns=['totalcost', '2Y value', 'Stocks IDs'])
 
     # Sort the dataframe to show the best combinations at the top
     df_sorted_options = df_options.sort_values(
-        by=['2 Year ROI'], ascending=False)
+        by=['2Y value'], ascending=False)
 
     # Export the table of sorted options to a csv file
     df_sorted_options.to_csv('output_options.csv')
--- optimized.py
+++ optimized.py
@@ -1,39 +1,69 @@
-from itertools import combinations
 import pandas as pd
 import time
 
 
 def main():
+
     column_names = ['action id', 'action cost', '2y benefit (%)']
     df = pd.read_csv("stock_list.csv", index_col=False, names=column_names)
 
-    df['2y value'] = df['action cost'] + \
-        (df['action cost'] * df['2y benefit (%)']/100)
-    df['2y ROI'] = df['2y value'] - df['action cost']
-    options = []
-    stocks_dict = df.to_dict('records')
-    # quadratic complexity O(n^2)
-    for L in range(0, len(stocks_dict)+1):
-        for subset in combinations(stocks_dict, L):
-            stocks = list(subset)
-            # O(n)
-            cost = sum(stock['action cost'] for stock in stocks)
-            # O(n)
-            value = sum(stock['2y value'] for stock in stocks)
-            # O(n)
-            roi = sum(stock['2y ROI'] for stock in stocks)
-            # O(n)
-            stocks_ids = [stock['action id'] for stock in stocks]
-            # O(n)
-            option = {'totalcost': cost,
-                      '2 Year ROI': roi,
-                      'Stocks IDs': stocks_ids}
-            if cost < 500:
-                options.append(option)
-    df_options = pd.DataFrame(
-        options, columns=['totalcost', '2 Year ROI', 'Stocks IDs'])
-    print(df_options.sort_values(by=['2 Year ROI'], ascending=False))
-    df_options.to_csv('output_options.csv')
+    # This is the memoization approach of
+    # 0 / 1 Knapsack in Python in simple
+    df['2y value'] = round(df['action cost'] * df['2y benefit (%)']/100, 2)
+    # driver code
+    val = df['2y value'].tolist()
+    wt = df['action cost'].tolist()
+    W = 500
+    n = len(val)
+
+    best_stocks = []
+
+    K = [[0 for w in range(W + 1)]
+         for i in range(n + 1)]
+
+    # Creating a table of n*W
+    # Fill each row (item) with the best value possible
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif wt[i - 1] <= w:
+                K[i][w] = round(max(val[i - 1]
+                                    + K[i - 1][w - wt[i - 1]],
+                                    K[i - 1][w]), 2)
+            else:
+                K[i][w] = K[i - 1][w]
+
+    # stores the result
+    # The value of K[n][W] (last row last column) is the highest
+    res = round(K[n][W], 2)
+    print("Best 2y ROI is: ", res)
+
+    # Now lets got through the table to find which items
+    # compose the highest value
+    w = W
+    for i in range(n, 0, -1):  # Starting at the bottom
+        # if res is 0, there are no more items possible
+        if res <= 0:
+            break
+        # Checks the cell on the top, if the value is the same as
+        # the current cell, move up
+        # otherwise add the current item (row), remove the value
+        # of the item, find the next best item with this value available
+        # in the table
+        if res == K[i - 1][w]:
+            continue
+        else:
+
+            # This item is included.
+            best_stocks.append(i)
+
+            # Since this weight is included
+            # its value is deducted
+            res = round(res - val[i - 1], 2)
+            w = w - wt[i - 1]
+
+    print("Best stocks IDs: ", best_stocks)
 
 
 if __name__ == "__main__":
