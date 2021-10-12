9ea7931ed7b5e51e2c0b21534e1829070c12004a
tom gauthier <tomgauthier0@gmail.com>
2021-10-12 09:43:09 +0200

    algorithm seems ok, but I get a different total ROI than Sienna on HER stocks selection in dataset2

--- optimized.py
+++ optimized.py
@@ -9,25 +9,32 @@ import time
 def main():
     # dataset = "stock_list.csv"
     dataset = sys.argv[1]
+
     W = int(sys.argv[2])
     print(W)
     W = W*100
+    # dataset = "dataset1_Python+P7.csv"
     # dataset = "dataset2_Python+P7.csv"
     df = pd.read_csv(dataset, index_col=False)
 
     # cleaning up the dataset
     # removing rows where price = 0
-    df = df[df.price != 0]
+    df = df[df.price >= 0]
 
     # reverting negative values
-    df['price'] = df['price'].abs()
+    df['price'] = df['price']*100
+    df['price'] = df['price'].astype(int)
     # This is the memoization approach of
     # 0 / 1 Knapsack in Python in simple
-    df['2y value'] = round(df['price'] * df['profit']/100, 2)
+    df['profit'] = df['profit']*100
+    df['profit'] = df['profit'].astype(int)
+
+    df['2y value'] = df['price'] * df['profit'] / 100
+    df['2y value'] = df['2y value'].astype(int)
 
     ids = df['name'].tolist()
-    val = [int(i * 100) for i in df['2y value'].tolist()]
-    wt = [int(i * 100) for i in df['price'].tolist()]
+    val = [i for i in df['2y value'].tolist()]
+    wt = [i for i in df['price'].tolist()]
     print(val)
     print(wt)
 
@@ -70,7 +77,7 @@ def main():
     # stores the result
     # The value of K[n][W] (last row last column) is the highest
     res = round(K[n][W], 2)
-    print("Best 2y ROI is: ", res/100)
+    print("Best 2y ROI is: ", res/10000)
 
     # Now lets got through the table to find which items
     # compose the highest value
@@ -90,7 +97,6 @@ def main():
 
             # This item is included.
             best_stocks.append(i - 1)
-            total_cost += K[i - 1][w]
             # Since this weight is included
             # its value is deducted
             res = round(res - val[i - 1], 2)
@@ -98,13 +104,10 @@ def main():
 
     print("Best stocks IDs: ", [ids[i] for i in best_stocks])
     print(best_stocks)
-    for stock_id in best_stocks:
-        stock = ids[stock_id]
-        print(df.loc[df['name'] == stock]['price'].values[0])
-        stock_value = df.loc[df['name'] == stock]['price'].values[0]
-        total_cost += stock_value
+    total_cost = sum(wt[i] for i in best_stocks)
+    print(total_cost)
 
-    print("total Cost :", total_cost)
+    print("total Cost :", total_cost/100)
 
 if __name__ == "__main__":
     start_time = time.time()
